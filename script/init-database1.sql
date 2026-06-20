18c1e8c7841c362f1f8382cf569c403cba50a911
yonaelananiya-1997 <yonaelananiya@gmail.com>
2026-06-20 12:24:29 -0700

    Update init-database.sql

--- script/init-database1.sql
+++ script/init-database1.sql
@@ -1,1 +1,17 @@
+use master
+
+if exists (select 1 from sys.databases where name='cbedatawarehouse')
+begin 
+alter database cbedatawarehouse set single_user with rollback immediate ; 
+
+ drop database cbedatawarehouse
+end;
+
+create database cbedatawarehosue;
+go 
+create schema bronze;
+go 
+create schema silver ;
+go 
+create schema gold ;
 
